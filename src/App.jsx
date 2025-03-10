eee0b764ec8a5bd012fb9f166883cf71c35bd216
crlsrgz <course@carlosfx.com>
2025-03-10 13:07:34 +0100

    INit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+
+import theData from './scenes/DAA-001/assets/Model.data.json';
+import { CabinetModel } from './cabinet/cabinet.model';
+
+// import { Perf } from "r3f-perf";
+export default function App() {
+  console.log(theData);
+  return (
+    <>
+      <main className="flex h-screen w-full flex-col overflow-x-hidden">
+        <section className="w-full bg-slate-500">
+          <h1 className="">Title {theData.name}</h1>{' '}
+          <span>
+            <img src="vite.svg" alt="" />
+          </span>
+          <p>
+            <a href="/001/index.html">link 001</a>
+          </p>
+        </section>
+
+        <section className="relative h-full w-full bg-slate-200">
+          <Canvas
+            style={{ width: '100%', height: '100%' }}
+            camera={{
+              position: [2, 1, 3],
+              near: 0.01,
+              far: 100,
+              fov: 45,
+            }}
+          >
+            <color attach="background" args={['#cccccc']} />
+            {/* <Perf></Perf> */}
+            <Environment preset="city" resolution={256} />
+
+            {/* <ambientLight intensity={0.5} />
+            <directionalLight position={(1, 1, 2)} /> */}
+            <OrbitControls />
+            <CabinetModel />
+          </Canvas>
+          <div
+            id="element-list"
+            className="fixed bottom-0 flex h-12 w-screen flex-row gap-6 bg-slate-500"
+          >
+            <button id="element-list-back">back</button>
+            <button id="element-list-back">forward</button>
+          </div>
+          {/* </div> */}
+        </section>
+      </main>
+    </>
+  );
+}
---
+++ src/cabinet/cabinet.model.jsx
@@ -0,0 +1,100 @@
+import { Edges, Html, Outlines, useGLTF } from '@react-three/drei';
+// import { Perf } from "r3f-perf";
+import { useEffect, useState } from 'react';
+import { Vector3 } from 'three';
+import { MeshBasicMaterial } from 'three';
+
+export function CabinetModel() {
+  const [hovered, setHover] = useState('');
+  const [seleceted, setSelected] = useState('');
+  const { nodes, materials } = useGLTF('/Cabinet.glb');
+  const altMat = new MeshBasicMaterial({ color: 'red' });
+  const materialSelected = new MeshBasicMaterial({ color: 'green' });
+
+  let keys = Object.keys(nodes);
+
+  // Prepare a list for rearange position
+  const listSteps = () => {
+    const arr = [];
+    keys.forEach((item) => {
+      const tmp = item.substring(0, item.indexOf('-'));
+      if (!arr.includes(tmp)) {
+        arr.push(tmp);
+      }
+    });
+    return arr.sort();
+  };
+
+  console.log('--', listSteps());
+  // Prepare a list for rearange position
+  const testFirstPos = new Vector3(2, 2, 2);
+  const copyFirst = Object.assign({}, testFirstPos);
+  const testSecondPos = new Vector3(3, 3, 3);
+  const testThirdPos = new Vector3(3, 3, 3);
+
+  console.log(testFirstPos.multiply(testSecondPos));
+  console.log(testFirstPos);
+  console.log(copyFirst);
+
+  useEffect(() => {
+    const elementList = document.querySelector('#element-list');
+    const list = listSteps();
+    list.forEach((item) => {
+      const div = document.createElement('button');
+      div.textContent = item;
+      elementList.appendChild(div);
+    });
+  }, []);
+  return (
+    <>
+      {/* <Perf position="bottom-right" /> */}
+      {keys.map((key) => {
+        if (key !== 'Scene') {
+          return (
+            <mesh
+              key={key}
+              castShadow
+              receiveShadow
+              geometry={nodes[key].geometry}
+              position={nodes[key].position}
+              material={
+                key === hovered
+                  ? altMat
+                  : key === seleceted
+                    ? materialSelected
+                    : materials['Material.001']
+              }
+              // material={materials["Material.001"]}
+              onPointerOver={(e) => {
+                e.stopPropagation();
+                setHover(key);
+                // console.log(key);
+              }}
+              onPointerOut={(e) => {
+                e.stopPropagation();
+                setHover('');
+              }}
+              onClick={(e) => {
+                e.stopPropagation();
+                setHover('');
+                setSelected(key);
+              }}
+              onPointerMissed={(e) => {
+                e.stopPropagation();
+                setHover('');
+                setSelected('');
+              }}
+            >
+              <Edges
+                color={
+                  key === hovered ? 'blue' : key === seleceted ? 'red' : 'black'
+                }
+                lineWidth={key === hovered ? 4 : key === seleceted ? 6 : 1}
+              ></Edges>
+            </mesh>
+          );
+        }
+      })}
+    </>
+  );
+}
