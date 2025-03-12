ebc82939bf7c3c2a89744ac6ca639bca0ae7ff55
crlsrgz <course@carlosfx.com>
2025-03-12 13:25:37 +0100

    feat:: restructure the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import DetailWrapper from './components/DetailWrapper.component';
+import DetailsList from './components/DetailsList.component';
+import NavBarDesktop from './components/NavBarDesktop.component';
 
-// import { Perf } from "r3f-perf";
 export default function App() {
   return (
     <>
       <main className="flex h-screen w-full flex-col overflow-x-hidden">
-        <section className="w-full bg-slate-500">
-          <h1 className="">Title </h1>{' '}
-          <span>
-            <img src="vite.svg" alt="" />
-          </span>
-          <p>
-            <a href="/001/index.html">link 001</a>
-          </p>
+        <section className="w-full">
+          <NavBarDesktop />
         </section>
 
-        <section className="relative h-full w-full bg-slate-200">
-          {/* <DetailWrapper productPath={'./scenes/DAA-003'} /> */}
-          <DetailWrapper productPath={'/001'} />
+        <section id="main-content" className="relative w-full bg-agave-800">
+          <DetailsList />
         </section>
+        <section className="w-full bg-limon-500 h-12">footer Info</section>
       </main>
     </>
   );
