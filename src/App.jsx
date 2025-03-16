720cc6e427d0d9abf588ba1977cebcc677c9f56c
crlsrgz <course@carlosfx.com>
2025-03-16 14:11:47 +0100

    feat: prepare single element file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import { useState } from 'react';
+import { CabinetModel } from './components/Cabinet.compontent';
 import DetailsList from './components/DetailsList.component';
+import InfoBar from './components/InfoBar.component';
 import NavBarDesktop from './components/NavBarDesktop.component';
 
 export default function App() {
+  const [cameraPosition, setCameraPosition] = useState([2, 1, 3]);
   return (
     <>
       <main className="flex h-screen w-full flex-col overflow-x-hidden">
-        <section className="w-full">
-          <NavBarDesktop />
-        </section>
-
-        <section id="main-content" className="relative w-full bg-agave-800">
-          <DetailsList />
+        <InfoBar />
+        <section className="relative h-full w-full bg-agave-900">
+          <CabinetModel canvasCameraPosition={cameraPosition} />
         </section>
         <section className="w-full bg-limon-500 h-12">footer Info</section>
       </main>
---
+++ src/components/InfoBar.component.jsx
@@ -0,0 +1,9 @@
+export default function InfoBar() {
+  return (
+    <>
+      <div className="w-20 fixed left-0 top-0 h-full bg-agave-600 z-10">
+        item
+      </div>
+    </>
+  );
+}
