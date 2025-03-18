e5c103e78b638a9076871778964032571f96f1e1
crlsrgz <course@carlosfx.com>
2025-03-18 13:10:31 +0100

    feat:single: footer layout

--- single_app/src/App.jsx
+++ single_app/src/App.jsx
@@ -3,6 +3,11 @@ import { CabinetModel } from './components/Cabinet.compontent';
 import InfoBar from './components/InfoBar.component';
 import InfoFooter from './components/InfoFooter.component';
 
+
+// Load data
+const response = await fetch("/model-data.json")
+const modelData = await response.json()
+
 export default function App() {
   const [cameraPosition, setCameraPosition] = useState([2, 1, 3]);
   const [open, setOpen] = useState(true);
@@ -15,7 +20,7 @@ export default function App() {
           <CabinetModel canvasCameraPosition={cameraPosition} />
         </section>
         <section className="w-full" >
-          <InfoFooter openState={open} switchOpenState={()=>{setOpen(!open)}}  title="click me"/>
+          <InfoFooter openState={open} switchOpenState={()=>{setOpen(!open)}}  title="click me" data={modelData}/>
         </section>
       </main>
     </>
--- single_app/src/components/InfoFooter.component.jsx
+++ single_app/src/components/InfoFooter.component.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react';
-export default function InfoFooter({openState, switchOpenState, title}) {
+export default function InfoFooter({openState, switchOpenState, title, data}) {
   return (
-    <>
       <div
-        className={`fixed inset-x-0 h-36 -bottom-0 bg-limon-500 z-20 transition-all duration-700 ease-out ${openState ? 'translate-y-0' : 'translate-y-24'}`}
+        id="info-footer-container"
+        className={`fixed inset-x-0 h-36 -bottom-0 flex flex-row gap-8 bg-limon-500 z-20 transition-all duration-700 ease-out ${openState ? 'translate-y-0' : 'translate-y-24'}`}
       >
-        <button
-          onClick={switchOpenState}
-          className="text-2xl w-full  text-center top-4 fixed"
-        >
-          {title??"Missing title"}
-        </button>
+        <div className='infoFooterSection'>
+          <div className='infoFooterField'>
+            {data.name?? "missing name from data"}
+          </div>
+        </div>
+
+        <div  className='infoFooterSection flex flex-col'>
+          <div className='infoFooterField'>
+            {data.name?? "missing name from data"}
+          </div>
+          <div className='infoFooterField'>
+            {data.name?? "missing name from data"}
+          </div>
+        </div>
+        <div  className='infoFooterSection flex flex-col'>
+          <div className='infoFooterField'>
+            {data.name?? "missing name from data"}
+          </div>
+          <div className='infoFooterField'>
+            {data.name?? "missing name from data"}
+          </div>
+        </div>
+        <div className='infoFooterSection'>
+          <div className='infoFooterField'>
+            <button onClick={switchOpenState} className="text-2xl w-full  text-center">
+              {title??"Missing title"}
+            </button>
+          </div>
+        </div>
       </div>
-    </>
   );
 }
