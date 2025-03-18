a88da4dce55f76e046516e99d3135867fe6c735d
crlsrgz <course@carlosfx.com>
2025-03-18 13:25:52 +0100

    feat:single: footer layout columns

--- single_app/src/components/InfoBar.component.jsx
+++ single_app/src/components/InfoBar.component.jsx
@@ -1,11 +1,11 @@
 export default function InfoBar() {
   return (
     <>
-      <div className=" flex flex-col justify-start items-center gap-2 content-center w-20 fixed left-0 top-0 h-full bg-agave-600 z-10">
-        <div className="block w-16 h-16 border-solid  border-2 border-limon-400"></div>
-        <div className="block w-16 h-16 border-solid  border-2 border-limon-400"></div>
-        <div className="block w-16 h-16 border-solid  border-2 border-limon-400"></div>
-        <div className="block w-16 h-16 border-solid  border-2 border-limon-400"></div>
+      <div className="fixed left-0 top-0 z-10 flex h-full w-20 flex-col content-center items-center justify-start gap-2 bg-agave-600">
+        <div className="block h-16 w-16 border-2 border-solid border-limon-400"></div>
+        <div className="block h-16 w-16 border-2 border-solid border-limon-400"></div>
+        <div className="block h-16 w-16 border-2 border-solid border-limon-400"></div>
+        <div className="block h-16 w-16 border-2 border-solid border-limon-400"></div>
       </div>
     </>
   );
--- single_app/src/components/InfoFooter.component.jsx
+++ single_app/src/components/InfoFooter.component.jsx
@@ -1,38 +1,46 @@
-export default function InfoFooter({openState, switchOpenState, title, data}) {
+export default function InfoFooter({
+  openState,
+  switchOpenState,
+  title,
+  data,
+}) {
   return (
-      <div
-        id="info-footer-container"
-        className={`fixed inset-x-0 h-36 -bottom-0 flex flex-row gap-8 bg-limon-500 z-20 transition-all duration-700 ease-out ${openState ? 'translate-y-0' : 'translate-y-24'}`}
-      >
-        <div className='infoFooterSection'>
-          <div className='infoFooterField'>
-            {data.name?? "missing name from data"}
-          </div>
-        </div>
-
-        <div  className='infoFooterSection flex flex-col'>
-          <div className='infoFooterField'>
-            {data.name?? "missing name from data"}
-          </div>
-          <div className='infoFooterField'>
-            {data.name?? "missing name from data"}
-          </div>
-        </div>
-        <div  className='infoFooterSection flex flex-col'>
-          <div className='infoFooterField'>
-            {data.name?? "missing name from data"}
-          </div>
-          <div className='infoFooterField'>
-            {data.name?? "missing name from data"}
-          </div>
-        </div>
-        <div className='infoFooterSection'>
-          <div className='infoFooterField'>
-            <button onClick={switchOpenState} className="text-2xl w-full  text-center">
-              {title??"Missing title"}
-            </button>
-          </div>
+    <div
+      id="info-footer-container"
+      className={`fixed inset-x-0 -bottom-0 z-20 flex h-36 flex-row gap-8 bg-limon-500 p-4 transition-all duration-700 ease-out ${openState ? 'translate-y-0' : 'translate-y-24'}`}
+    >
+      <div className="info-footer-section w-1/6">
+        <div className="info-footer-field">
+          {data.name ?? 'missing name from data'}
         </div>
       </div>
+
+      <div className="info-footer-section flex w-2/6 flex-col text-left">
+        <div className="info-footer-field">
+          {data.name ?? 'missing name from data'}
+        </div>
+        <div className="info-footer-field">
+          {data.name ?? 'missing name from data'}
+        </div>
+      </div>
+      <div className="info-footer-section flex w-2/6 flex-col">
+        <div className="info-footer-field">
+          {data.name ?? 'missing name from data'}
+        </div>
+        <div className="info-footer-field">
+          {data.name ?? 'missing name from data'}
+        </div>
+      </div>
+      <div className="info-footer-section w-1/6">
+        <div className="info-footer-close -translate-y-2">
+          <button
+            onClick={switchOpenState}
+            className="w-full text-center text-2xl"
+          >
+            {title ?? 'Missing title'}
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
