e363fac0578909b33fd96abfcd2ebb115a1b69d5
crlsrgz <course@carlosfx.com>
2025-03-30 15:33:19 +0200

    func: testing jsx glb

--- single_app/src/components/Cabinet.compontent.jsx
+++ single_app/src/components/Cabinet.compontent.jsx
@@ -2,10 +2,10 @@ import { OrbitControls, Environment, useAnimations } from '@react-three/drei';
 import { Edges, Html, Outlines, useGLTF } from '@react-three/drei';
 // import { Perf } from "r3f-perf";
 import { useEffect, useRef, useState } from 'react';
-import { Material, Vector3 } from 'three';
-import { MeshBasicMaterial } from 'three';
+import { Material, Vector2, Vector3 } from 'three';
+import { MeshBasicMaterial, Raycaster } from 'three';
 
-import model from '../assets/models/Cabinet.glb?url';
+import modelURL from '../assets/models/Cabinet.glb?url';
 import logInfo from '../utils/logger';
 import { useControls } from 'leva';
 
@@ -23,31 +23,18 @@ const edgeColorSelected = 'dodgerblue';
 export function CabinetModel(props) {
   const [hovered, setHover] = useState('');
   const [selected, setSelected] = useState('');
-  const { nodes, materials, animations, scene } = useGLTF(model);
+  const { nodes, materials, animations } = useGLTF(modelURL);
+  const group = useRef();
+  const { actions } = useAnimations(animations, group);
 
   /**
    * Animations
    */
-  const actions = useAnimations(animations, scene);
-  // console.log('actions', actions);
-  // console.log('actions names', actions.names);
-
-  const { actionName } = useControls({
-    actionName: { options: actions.names },
-  });
   useEffect(() => {
-    const runAction = actions.actions[actionName];
-    // const runAction = actions.actions[actionName];
-    console.log('run action', runAction, actions.actions, actionName);
-    runAction.reset().fadeIn(0.5).play();
-
-    //Cleanup in useEffect
-    return () => {
-      runAction.fadeOut(0.5);
-      console.log('dispose');
-    };
-  }, [actionName]);
-
+    console.log(actions);
+    actions[animations[0].name].play();
+    actions[animations[1].name].play();
+  }, []);
   /**
    * Update the sting and send to parent component
    * @param {string} s
@@ -58,31 +45,9 @@ export function CabinetModel(props) {
     // logInfo('sent from child', newString);
   };
 
-  let keys = Object.keys(nodes);
-
-  // Prepare a list for rearange position
-  // const listSteps = () => {
-  //   const arr = [];
-  //   keys.forEach((item) => {
-  //     const tmp = item.substring(0, item.indexOf('-'));
-  //     if (!arr.includes(tmp)) {
-  //       arr.push(tmp);
-  //     }
-  //   });
-  //   return arr.sort();
-  // };
-
-  // console.log('--list steps', listSteps());
-
-  // // Prepare a list for rearange position
-  // const testFirstPos = new Vector3(2, 2, 2);
-  // const copyFirst = Object.assign({}, testFirstPos);
-  // const testSecondPos = new Vector3(3, 3, 3);
-
-  // console.log(testFirstPos.multiply(testSecondPos));
-  // console.log(testFirstPos);
-  // console.log(copyFirst);
-
+  /**
+   * Selection
+   */
   /**
    * @todo Solution to go back to the original material after switch to hover,
    * @todo create a fallback material in case the node does not have one.
@@ -99,62 +64,25 @@ export function CabinetModel(props) {
       <OrbitControls />
       <axesHelper args={[3]} />
       {/* <Perf position="bottom-right" /> */}
-      {keys.map((key) => {
-        if (key !== 'Scene') {
-          return (
-            <mesh
-              key={key}
-              castShadow
-              receiveShadow
-              geometry={nodes[key].geometry}
-              position={nodes[key].position}
-              material={
-                key === hovered
-                  ? materialHovered
-                  : key === selected
-                    ? materialSelected
-                    : materials[nodes[key].material?.name ?? 'nothing']
-              }
-              onPointerOver={(e) => {
-                e.stopPropagation();
-                setHover(key);
-              }}
-              onPointerOut={(e) => {
-                e.stopPropagation();
-                setHover('');
-              }}
-              onClick={(e) => {
-                e.stopPropagation();
-                setHover('');
-                setSelected(key);
-                console.log('click event', nodes[key].name);
-                // console.log(materialNameSelected);
-                handleStringUpdate(nodes[key].name);
-              }}
-              onPointerMissed={(e) => {
-                // console.log('event is', e);
-                if (e.type === 'click') {
-                  e.stopPropagation();
-                  setHover('');
-                  setSelected('');
-                  handleStringUpdate('');
-                }
-              }}
-            >
-              <Edges
-                color={
-                  key === hovered
-                    ? edgeColorHovered
-                    : key === selected
-                      ? edgeColorSelected
-                      : edgeColorStandard
-                }
-                lineWidth={key === hovered ? 4 : key === selected ? 6 : 1}
-              ></Edges>
-            </mesh>
-          );
-        }
-      })}
+      <group ref={group} {...props} dispose={null}>
+        <group name="Scene">
+          <mesh
+            name="Cube000"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube000.geometry}
+            material={materials.Material}
+          />
+          <mesh
+            name="Cube001"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube000.geometry}
+            material={materials.Material}
+            position={[0, 0, -1.435]}
+          />
+        </group>
+      </group>
     </>
   );
 }
