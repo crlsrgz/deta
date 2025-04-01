b9e109cc5ffe391518188582dcd614cd58817576
crlsrgz <course@carlosfx.com>
2025-04-01 13:20:47 +0200

    layout: Single: info box

--- single_app/src/components/InfoFooter.component.jsx
+++ single_app/src/components/InfoFooter.component.jsx
@@ -27,48 +27,54 @@ export default function InfoFooter({
     <div
       id="info-footer-container"
       aria-expanded={openState}
-      className={`fixed inset-x-0 -bottom-0 z-20 flex h-56 flex-row gap-8 bg-limon-500 px-4 py-8 transition-all duration-700 ease-out md:px-16 lg:px-56 ${openState ? 'translate-y-0' : 'translate-y-36'}`}
+      className={`fixed inset-x-0 -bottom-0 z-20 flex h-56 flex-row gap-8 bg-limon-500 px-4 py-8 transition-all duration-700 ease-out md:px-16 lg:px-40 xl:px-56 ${openState ? 'translate-y-0' : 'translate-y-48'}`}
     >
-      <div className="info-footer-section w-1/6" tabIndex={5}>
-        <div className="info-footer-field">
-          {detailData.name ?? 'missing name from data'}
-        </div>
-      </div>
+      <figure className="info-footer-section w-1/12" tabIndex={21}>
+        <div className="h-24 w-24 border-4 border-solid border-agave-700"></div>
+      </figure>
 
       <ul
-        className="info-footer-section flex w-2/6 flex-col text-left"
-        tabIndex={15}
+        className="info-footer-section flex w-6/12 flex-col text-left"
+        tabIndex={31}
       >
-        <li className="info-footer-field-title text-4xl" tabIndex={-1}>
+        <li
+          className="font-anybody info-footer-field-title text-4xl"
+          tabindex="-1"
+        >
           {detailData.name ?? 'missing name from data'}
         </li>
-        <li className="info-footer-field" tabIndex={-1}>
-          {detailData.name ?? 'missing name from data'}
+        <li className="info-footer-field" tabindex="-1">
+          {detailData.description ?? 'missing name from data'}
         </li>
       </ul>
-      <ul className="info-footer-section flex w-2/6 flex-col" tabIndex={20}>
-        <li className="info-footer-field">
+      <ul
+        className="info-footer-section font-jakartaRegular flex w-4/12 flex-col"
+        tabIndex={41}
+        role={'listbox'}
+      >
+        <li className="info-footer-field" tabindex="-1">
           <span>
             Element: {materialData[language].name ?? 'missing name from data'}
           </span>
         </li>
-        <li className="info-footer-field">
-          <span>Material:</span>
+        <li className="info-footer-field" tabindex="-1">
           <span>
+            Material:{' '}
             {materialData[language].materialColloquial ??
               'missing name from data'}
           </span>
         </li>
-        <li className="info-footer-field">
-          <span>Decription:</span>
+        <li className="info-footer-field" tabindex="-1">
           <span>
+            Decription:{' '}
             {materialData[language].extract ?? 'missing name from data'}
           </span>
         </li>
       </ul>
-      <div className="info-footer-section w-1/6">
-        <div className="info-footer-close z-30 -translate-y-2">
+      <div className="info-footer-section w-1/12">
+        <div className="info-footer-close z-30 -translate-y-8">
           <button
+            tabIndex={51}
             onClick={switchOpenState}
             className="w-full text-center text-2xl"
           >
